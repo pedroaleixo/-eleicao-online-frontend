c243f0333a6f892ffc4424bde7fd556ca57d457b
unknown <pedro.aleixo@gmail.com>
2021-12-03 00:24:08 -0300

    Correção do bug do botão de votar

--- src/app/features/eleitor/services/eleitor.service.ts
+++ src/app/features/eleitor/services/eleitor.service.ts
@@ -1,9 +1,19 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { environment } from 'src/app/environments/environment';
+import { Eleitor } from '../interfaces/eleitor';
+
+const API_URL = environment.apiUrl+'/api';
 
 @Injectable({
   providedIn: 'root'
 })
 export class EleitorService {
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+  public buscarEleitorPeloIdPessoa(idPessoa:number, idEleicao:number) : Observable<Eleitor>{
+    return this.http.get<Eleitor>(`${API_URL}/eleitor/pessoa/${idPessoa}/${idEleicao}`);
+  }
 }
--- src/app/pages/votacao-page/votacao-page.component.ts
+++ src/app/pages/votacao-page/votacao-page.component.ts
@@ -6,6 +6,10 @@ import { MatPaginator, PageEvent } from '@angular/material/paginator';
 import { EleicaoService } from 'src/app/features/eleicao/services/eleicao.service';
 import { UserService } from 'src/app/core/services/user.service';
 import { Router } from '@angular/router';
+import { EleitorService } from 'src/app/features/eleitor/services/eleitor.service';
+import { Observable } from 'rxjs';
+import { Eleitor } from 'src/app/features/eleitor/interfaces/eleitor';
+import { take } from 'rxjs/operators';
 
 @Component({
   selector: 'app-votacao-page',
@@ -20,11 +24,15 @@ export class VotacaoPageComponent implements AfterViewInit, OnInit {
   pageIndex!: number;
   pageSize!:number;
   length!:number;
+  pessoa!:number;
+  mapEleitorNaoVotou: Map<number, boolean> = new Map();
 
   @ViewChild(MatPaginator)paginator!: MatPaginator;
 
   constructor(private userService: UserService,
-    private eleicaoService: EleicaoService, private router: Router) { }
+    private eleicaoService: EleicaoService,
+    private eleitorService: EleitorService,
+    private router: Router) { }
 
 
   ngOnInit(): void {
@@ -35,12 +43,19 @@ export class VotacaoPageComponent implements AfterViewInit, OnInit {
   ngAfterViewInit() {
     this.userService.getUser().subscribe(user => {
       if(user?.pessoa) {
+        this.pessoa = user.pessoa;
         this.eleicaoService.listarEleicoesPorPessoaEleitor(user.pessoa, 0, 5)
         .subscribe(page => {
           this.dataSource.data = page.content;
           this.pageIndex = page.pageable.pageNumber;
           this.pageSize = page.pageable.pageSize;
           this.length = page.totalElements;
+
+          this.dataSource.data.forEach(eleicao => {
+            this.eleitorService.buscarEleitorPeloIdPessoa(this.pessoa, eleicao.id).subscribe(eleitor => {
+               this.mapEleitorNaoVotou.set(eleicao.id, eleitor.dataHoraVotou == null);
+            })
+          });
         });
       }
     });
@@ -63,12 +78,15 @@ export class VotacaoPageComponent implements AfterViewInit, OnInit {
     return event;
   }
 
-  isIniciada(eleicao:Eleicao){
-    if(eleicao && eleicao.situacao == SituacaoEleicao.INICIADA.valueOf()){
+
+  podeVotar(eleicao:Eleicao){
+    if(eleicao && eleicao.situacao == SituacaoEleicao.INICIADA.valueOf() && this.mapEleitorNaoVotou.get(eleicao.id)){
       return true;
     }
     return false;
   }
+
+
 
   votarEleicao(eleicao:Eleicao){
     this.router.navigate(['votacao/list/'+eleicao.id]);
