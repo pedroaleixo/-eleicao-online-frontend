8886ca4288675e53d8743ed194e409ea9840ba7a
unknown <pedro.aleixo@gmail.com>
2021-12-05 23:32:39 -0300

    Pequenos ajustes de interface

--- src/app/core/guards/auth-admin.guard.ts
+++ src/app/core/guards/auth-admin.guard.ts
@@ -9,12 +9,15 @@ import {
 import { Observable } from 'rxjs';
 import { TokenService } from '../services/token.service';
 import { UserService } from '../services/user.service';
+import { StorageService } from '../services/storage.service';
+import { ELEICAO_ATIVA } from '../util/constants';
 
 @Injectable({ providedIn: 'root' })
 export class AuthAdminGuard implements CanActivate {
   constructor(
     private userService: UserService,
     private tokenService: TokenService,
+    private storageService: StorageService,
     private snackbarService: SnackbarService,
     private router: Router
   ) {}
@@ -27,6 +30,13 @@ export class AuthAdminGuard implements CanActivate {
       this.router.navigate(['login-admin']);
       this.snackbarService.warning('Usuário não tem permissão para acessar o módulo de administração');
       return false;
+    } else if(this.userService.isComissao()){
+      this.userService.getUser().subscribe(user => {
+        if(user && user.eleicoes && user.eleicoes.length > 0){
+          this.storageService.setItem(ELEICAO_ATIVA, user.eleicoes[0]);
+        }
+      });
+
     }
     return true;
   }
