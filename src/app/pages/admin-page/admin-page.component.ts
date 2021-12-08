5a776ddf0f8a1f300dbd3c6fe1f7bc50cdc6b8f6
unknown <pedro.aleixo@gmail.com>
2021-12-08 20:58:13 -0300

    Ajuste da tela inicial de admin para comissao

--- src/app/pages/admin-page/admin-page.component.ts
+++ src/app/pages/admin-page/admin-page.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+import { UserService } from 'src/app/core/services/user.service';
 
 @Component({
   selector: 'app-admin-page',
@@ -8,13 +9,17 @@ import { Router } from '@angular/router';
 })
 export class AdminPageComponent implements OnInit {
 
-  constructor(private router: Router) { }
+  constructor(private userService:UserService, private router: Router) { }
 
   ngOnInit(): void {
   }
 
   irPara(page:string){
     this.router.navigate([page]);
+  }
+
+  isComissao(): boolean{
+    return this.userService.isComissao();
   }
 
 }
