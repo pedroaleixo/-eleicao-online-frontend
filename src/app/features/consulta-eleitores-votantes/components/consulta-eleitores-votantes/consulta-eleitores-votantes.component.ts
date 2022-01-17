041ae787345827a56c9e0ff57abc08d421c4f6f3
unknown <pedro.aleixo@gmail.com>
2022-01-16 21:34:18 -0300

    Implementação da tela de consulta de eleitores votantes

---
+++ src/app/features/consulta-eleitores-votantes/components/consulta-eleitores-votantes/consulta-eleitores-votantes.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-consulta-eleitores-votantes',
+  templateUrl: './consulta-eleitores-votantes.component.html',
+  styleUrls: ['./consulta-eleitores-votantes.component.css']
+})
+export class ConsultaEleitoresVotantesComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
---
+++ src/app/features/consulta-eleitores-votantes/consulta-eleitores-votantes.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { ConsultaEleitoresVotantesComponent } from './components/consulta-eleitores-votantes/consulta-eleitores-votantes.component';
+
+
+
+@NgModule({
+  declarations: [
+    ConsultaEleitoresVotantesComponent
+  ],
+  imports: [
+    CommonModule
+  ]
+})
+export class ConsultaEleitoresVotantesModule { }
